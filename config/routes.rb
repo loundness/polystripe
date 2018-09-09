8deb9ca390923bd2db165bf8bb768080411aeb93
loundness <ludo.vic@me.com>
2018-09-09 07:59:51 +0200

    config stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :charges, only:[:new, :create]
+ resources :charges, only: [:new, :create]
+  root 'charges#new'
+  post '/charges',  to: 'charges#create'
 
+  post 'subscribe' => 'charges#subscribe'
 end
